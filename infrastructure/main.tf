e09451b894454305c2ad8d83764a56d4ad3ab5e1
Evandro Pomatti <evandro.pomatti@gmail.com>
2021-08-22 11:47:12 -0300

    dns auto-registration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -106,6 +106,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "cosmos" {
   resource_group_name   = azurerm_resource_group.default.name
   private_dns_zone_name = azurerm_private_dns_zone.cosmos.name
   virtual_network_id    = azurerm_virtual_network.default.id
+  registration_enabled  = true
 }
 
 # Database
